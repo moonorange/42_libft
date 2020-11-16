8349725efecf0804989d5033f970539f71100e1d
moonorange <monoma632@gmail.com>
2020-11-16 22:44:50 +0900

    add: lstclear

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,8 +6,19 @@
 /*   By: kkida <kkida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 13:23:06 by kkida             #+#    #+#             */
-/*   Updated: 2020/11/15 13:35:57 by kkida            ###   ########.fr       */
+/*   Updated: 2020/11/16 21:28:18 by kkida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	if (!lst || !del)
+		return ;
+	if (!*lst)
+		return ;
+	ft_lstclear(&((*lst)->next), del);
+	ft_lstdelone(*lst, del);
+	*lst = NULL;
+}
