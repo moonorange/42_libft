4d87a80603474f96bc298fe485511d863648680d
moonorange <monoma632@gmail.com>
2020-11-16 20:36:02 +0900

    add: lst add last

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,8 +6,23 @@
 /*   By: kkida <kkida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 13:22:58 by kkida             #+#    #+#             */
-/*   Updated: 2020/11/15 19:23:24 by kkida            ###   ########.fr       */
+/*   Updated: 2020/11/16 20:35:18 by kkida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list		*lst_el;
+
+	if (!lst)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		lst_el = ft_lstlast(*lst);
+		lst_el->next = new;
+	}
+}
