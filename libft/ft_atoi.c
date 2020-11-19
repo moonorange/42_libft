1dfd7c25b54e11a3d0f9d069c770324940a5eb45
moonorange <monoma632@gmail.com>
2020-11-19 19:01:29 +0900

    fix: ft_atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: kkida <kkida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 19:52:10 by kkida             #+#    #+#             */
-/*   Updated: 2020/11/18 19:31:45 by kkida            ###   ########.fr       */
+/*   Updated: 2020/11/19 19:00:33 by kkida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ int		ft_atoi(const char *str)
 {
 	long long int	ans;
 	int				sign;
-	long int		digit;
 	long long int	ov_flow;
 
 	sign = 1;
-	digit = 0;
 	if (!*str)
 		return (0);
 	while (ft_isspace(*str))
@@ -35,12 +33,12 @@ int		ft_atoi(const char *str)
 	ans = 0;
 	while (ft_isdigit(*str))
 	{
-		digit++;
 		ov_flow = ans * 10 + (*str - '0');
 		if (ov_flow < ans)
 			return ((sign + 1) / (-2));
 		else
 			ans = ov_flow;
+		str++;
 	}
 	return (int)(ans * sign);
 }
