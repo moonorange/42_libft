c26bd29db7402924e5f15e7d28adf9e7ff3c4c8e
moonorange <monoma632@gmail.com>
2020-11-20 19:42:57 +0900

    fix: libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: kkida <kkida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 22:27:31 by kkida             #+#    #+#             */
-/*   Updated: 2020/11/18 18:49:26 by kkida            ###   ########.fr       */
+/*   Updated: 2020/11/20 19:42:34 by kkida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-#include <limits.h>
+# include <limits.h>
 
 typedef struct		s_list
 {
